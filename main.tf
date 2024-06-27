945f63dfa455c57e325edf21b8a57739321afcfc
Delight Fela-Steve <dfelastevegit@gmail.com>
2024-06-27 18:40:51 +0100

    add ingress rule for port 80 for apache

--- main.tf
+++ main.tf
@@ -69,3 +69,11 @@ resource "aws_vpc_security_group_ingress_rule" "allow_web_port" {
   ip_protocol       = "tcp"
   to_port           = 8000
 }
+
+resource "aws_vpc_security_group_ingress_rule" "allow_apache_port" {
+  security_group_id = aws_security_group.allow_web_port.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
